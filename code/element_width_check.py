b55d41f24983e20ac004483d53de33ce6cb7eeaf
Annie <ansross@cs.washington.edu>
2018-03-14 10:31:15 -0700

    witdh and height checks appear to work

---
+++ code/element_width_check.py
@@ -0,0 +1,38 @@
+from check_base import Check_Base 
+import talkback_accessible
+from element_height_check import px_to_dp
+
+class Element_Width_Check(Check_Base):
+
+	def __init__(self, id_arg, node_arg):
+		Check_Base.__init__(self,id_arg, node_arg)
+
+	# this check only checks if it has a label, that is, if it's label (
+	# be it personal label or from children, is not None
+
+	def perform(self):
+		### Label check
+		# if talkback accessible, should have appropriate label
+		self.result = self.__wide_enough()
+
+	#####
+	### Check Width
+	#####
+
+	def __wide_enough(self):
+		#k = self.node.raw_properties.keys()
+		# bounds is 4 node array
+		# 0 = left, 1 = top, 2 = right, 3 = bottom
+		# google dev guidelines, needs to be 48 dp
+		bounds = self.node.get_bounds()
+		px_width = bounds['right'] - bounds['left']
+		# must convert pixel to dp
+
+		dp_width = px_to_dp(px_width)
+		dp_width_threshold = 48
+		wide_enough = None
+		if (dp_width < dp_width_threshold ):
+			wide_enough = False
+		else:
+			wide_enough = True
+		return wide_enough
--- code/node_checker.py
+++ code/node_checker.py
@@ -1,5 +1,6 @@
 from speakable_text_present_check import Speakable_Text_Present_Check
-
+from element_height_check import Element_Height_Check
+from element_width_check import Element_Width_Check
 
 class Node_Checker:
     def __init__(self, node_arg):
@@ -8,25 +9,30 @@ class Node_Checker:
         self.log = []
 
         # list of checks to perform on node
-        # map id to CHECK object
-        self.checks = {}
+        # in list because want to ensure prints out in order
+        self.checks = []
         self.__initialize_checks()
 
     def __initialize_checks(self):
         # some tests only to be performed on talkback accessible nodes
         if self.node.is_talkback_accessible():
-            self.checks["Speakable_Text_Present"] = Speakable_Text_Present_Check(self.node)
+            self.checks.append(Speakable_Text_Present_Check("Speakable_Text_Present",self.node))
+            self.checks.append(Element_Width_Check("Element_Wide_Enough",self.node))
+            self.checks.append(Element_Height_Check("Element_Tall_Enough",self.node))
 
     def perform_checks(self):
-        for check in self.checks.values():
+        for check in self.checks:
             check.perform()
 
     def print_views_table(self):
         # print order:
         # has_speakable_text_present,
         if self.node.is_talkback_accessible():
-            print(str(self.checks['Speakable_Text_Present'].result) + ",", end="")
+            for c in self.checks:
+                print(str(c.result)+",",end="")
 
-
+## MUST BE IN SAME ORDER AS PUT IN __INITIALIZE_CHECKS
 def print_checker_header():
     print("Speakable_Text_Present,", end="")
+    print("Element_Wide_Enough,",end="")
+    print("Element_Tall_Enough,",end="")
